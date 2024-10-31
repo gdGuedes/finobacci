257a518133d4670068aeb0a3c8e37041a721d4e2
Guilherme Guedes <167482095+gdGuedes@users.noreply.github.com>
2024-10-31 14:04:55 -0300

    Add files via upload

---
+++ Finobacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class FibonacciCheck
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsFibonacci(number))
+        {
+            Console.WriteLine($"O número {number} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {number} NÃO pertence à sequência de Fibonacci.");
+        }
+    }
+
+    static bool IsFibonacci(int number)
+    {
+        int a = 0, b = 1;
+
+        while (a < number)
+        {
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        return a == number;
+    }
+}
